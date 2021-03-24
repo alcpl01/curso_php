149b5f113747dc252af9215442967e225ea11ba9
Ana Luiza Campos <lindinaluli12@gmail.com>
2021-03-24 11:35:41 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+    $host = 'localhost';
+    $user = 'root';
+    $passwd = '';
+    $bd_name = 'teste_php';
+
+    $connx = mysqli_connect($host, $user, $passwd, $bd_name);
+
+    //if($connx){
+    //    echo 'Conectou';
+    //}else{
+    //    echo 'erro';
+    //}
+
+
+?>
